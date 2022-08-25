3aaa6f6eba4ddccbb6cb2961a98901a8be1e0fc1
Faddey Kryachko <faddeyca@gmail.com>
2022-08-25 22:55:48 +0500

    almost work

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from math import atan2, pi
 import pygame
 import sys
 
@@ -10,10 +11,10 @@ moves4 = [[-1, 0], [0, -1], [0, 1], [1, 0]]
 
 
 class Game:
-    def __init__(self):
-        self.up_length = 100
-        self.linesX = 20
-        self.linesY = 20
+    def __init__(self, linesX, linesY):
+        self.linesX = linesX
+        self.linesY = linesY
+
         self.block_size = 25
         self.up_length = 100
         self.gap = 50
@@ -26,6 +27,8 @@ class Game:
         self.table =  [[-1] * self.linesY for i in range(self.linesX)]
         self.components_counter = [0, 0]
         self.used = []
+        self.components = dict()
+        self.dots = [[], []]
 
         self.score = [0, 0]
 
@@ -113,10 +116,15 @@ class Game:
     def fill_component(self, pos, val):
         x = pos[0]
         y = pos[1]
-        self.table[x][y] = val
+        if val == -2:
+            current = self.table[x][y] % 2
+            if pos in self.dots[current]:
+                self.dots[current].remove(pos)
         dots = self.get_neighbours(pos)
+        self.table[x][y] = val
         for dot in dots:
-            if self.table[dot[0]][dot[1]] != val:
+            comp = self.table[dot[0]][dot[1]]
+            if comp != val:
                 self.fill_component(dot, val)
 
     def get_next_component_value(self, current):
@@ -133,107 +141,124 @@ class Game:
                 for y in range(self.linesY):
                     if self.table[x][y] == old:
                         new = self.get_next_component_value(old % 2)
+                        self.components[new] = (x, y)
                         self.fill_component((x, y), new)
                         flag = True
                         break
+            flag = False
+            for x in range(self.linesX):
+                if flag:
+                    break
+                for y in range(self.linesY):
+                    if self.table[x][y] == old:
+                        flag = True
+                        break
+            if not flag:
+                if old in self.components:
+                    del self.components[old]
 
-    def check(self, current):
-        if current == 91:
-            pass
+    def are_close(self, dot1, dot2):
+        return abs(dot1[0] - dot2[0]) <= 1 and abs(dot1[1] - dot2[1]) <= 1
+
+    def check_part_two(self, current):
+        x, y = self.components[current]
         enemy = (current + 1) % 2
-        component = []
-        left = self.linesX
-        right = 0
-        up = self.linesY
-        down = 0
-        for x in range(self.linesX):
-            for y in range(self.linesY):
-                if self.table[x][y] == current:
-                    component.append((x, y))
-                    left = min(left, x)
-                    right = max(right, x)
-                    up = min(up, y)
-                    down = max(down, y)
-        left -= 1
-        right += 1
-        up -= 1
-        down += 1
-        if left < 0 or right >= self.linesX or up < 0 or down >= self.linesY:
-            return
+        angles = dict()
+        min_dot = None
+        min_dist = None
+        for dot in self.dots[enemy]:
+            angle = atan2(dot[0] - x, dot[1] - y)
+            if angle not in angles:
+                dist = ((dot[0] - x) ** 2 + (dot[1] - y) ** 2) ** 0.5
+                angles[angle] = (dot, dist)
+                if min_dot is None:
+                    min_dot = angle
+                    min_dist = dist
+                else:
+                    if dist < min_dist:
+                        min_dist = dist
+                        min_dot = angle
+            else:
+                dist = ((dot[0] - x) ** 2 + (dot[1] - y) ** 2) ** 0.5
+                if dist < angles[angle][1]:
+                    angles[angle] = (dot, dist)
+                    if dist < min_dist:
+                        min_dist = dist
+                        min_dot = angle
 
-        for x in range(left, right + 1):
-            angry = False
-            opened = False
-            for y in range(up, down + 1):
-                if self.table[x][y] < 0:
-                    if not opened:
-                        angry = False
-                elif self.table[x][y] == current:
-                    if not angry:
-                        return
-                    else:
-                        opened = True
-                elif self.table[x][y] % 2 == enemy:
-                    angry = True
-                    opened = False
-        
-        for x in range(left, right + 1):
-            angry = False
-            opened = False
-            for y in reversed(range(up, down + 1)):
-                if self.table[x][y] < 0:
-                    if not opened:
-                        angry = False
-                elif self.table[x][y] == current:
-                    if not angry:
-                        return
-                    else:
-                        opened = True
-                elif self.table[x][y] % 2 == enemy:
-                    angry = True
-                    opened = False
+        if min_dot is None:
+            return -1
+        ans1 = [angles[min_dot][0]]
+        prev = min_dot
+        prev_dot = angles[min_dot][0]
+        for i in sorted(angles.keys()):
+            if i > min_dot:
+                if abs(prev - i) > pi / 2:
+                    return -1
+                dot = angles[i][0]
+                if self.are_close(prev_dot, dot):
+                    ans1.append(dot)
+                    prev = i
+                    prev_dot = dot
+        ans2 = []
+        prev = min_dot
+        prev_dot = angles[min_dot][0]
+        for i in reversed(sorted(angles.keys())):
+            if i < min_dot:
+                if abs(prev - i) > pi / 2:
+                    return -1
+                dot = angles[i][0]
+                if self.are_close(prev_dot, dot):
+                    ans2.append(dot)
+                    prev = i
+                    prev_dot = dot
 
-        for y in range(up, down + 1):
-            angry = False
-            opened = False
-            for x in range(left, right + 1):
-                if self.table[x][y] < 0:
-                    if not opened:
-                        angry = False
-                elif self.table[x][y] == current:
-                    if not angry:
-                        return
-                    else:
-                        opened = True
-                elif self.table[x][y] % 2 == enemy:
-                    angry = True
-                    opened = False
-        
-        for y in range(up, down + 1):
-            angry = False
-            opened = False
-            for x in reversed(range(left, right + 1)):
-                if self.table[x][y] < 0:
-                    if not opened:
-                        angry = False
-                elif self.table[x][y] == current:
-                    if not angry:
-                        return
-                    else:
-                        opened = True
-                elif self.table[x][y] % 2 == enemy:
-                    angry = True
-                    opened = False
-        
-        self.score[enemy] += len(component)
-        for x, y in component:
-            self.table[x][y] = -2
+        def check_circle(ans):
+            first = False
+            second = False
+            third = False
+            fourth = False
+            for xx, yy in ans:
+                if xx - x >= 0 and yy - y >= 0:
+                    first = True
+                if xx - x >= 0 and yy - y <= 0:
+                    second = True
+                if xx - x <= 0 and yy - y >= 0:
+                    third = True
+                if xx - x <= 0 and yy - y <= 0:
+                    fourth = True
+            return first and second and third and fourth
+
+        if len(ans2) > 0:
+            if len(ans1) + len(ans2) < 4:
+                return -1
+            while len(ans2) > 0 and not self.are_close(ans1[-1], ans2[-1]):
+                ans2.pop()
+            if len(ans2) != 0:
+                if self.are_close(ans1[-1], ans2[-1]):
+                    ans = ans1 + ans2
+                    if check_circle(ans):
+                        return ans
+                return -1
+        if len(ans1) < 4:
+            return -1
+        if self.are_close(ans1[0], ans1[-1]):
+            if check_circle(ans1):
+                return ans1
+        return -1
+            
+
 
     def put_dot(self, pos):
         if pos in self.used:
             return
 
-        self.table[pos[0]][pos[1]] = self.get_next_component_value(self.turn)
+        self.used.append(pos)
+        self.dots[self.turn].append(pos)
+
+        comp = self.get_next_component_value(self.turn)
+        self.table[pos[0]][pos[1]] = comp
+        self.components[comp] = pos
 
         to_refill = set()
         for ax, ay in moves8:
@@ -247,18 +272,12 @@ class Game:
 
         self.refill_components(to_refill)
 
-        to_check = set()
-        for ax, ay in moves8:
-            nx, ny = pos[0] + ax, pos[1] + ay
-            if nx < 0 or nx >= self.linesX or ny < 0 or ny >= self.linesY:
-                continue
-            if self.table[nx][ny] < 0:
-                continue
-            to_check.add(self.table[nx][ny])
-        to_check.add(self.table[pos[0]][pos[1]])
-
-        for comp in to_check:
-            self.check(comp)
+        for comp in list(self.components):
+            if comp in self.components:
+                res = self.check_part_two(comp)
+                if res != -1:
+                    self.fill_component(self.components[comp], -2)
+                    del self.components[comp]
 
         self.turn += 1
         self.turn %= 2
@@ -300,5 +319,5 @@ class Game:
 
 
 if __name__ == "__main__":
-    game = Game()
+    game = Game(20, 20)
     game.start()
